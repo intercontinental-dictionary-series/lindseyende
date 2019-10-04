948446fd38336150cc1cf2ff349be6e2291de9fe
xrotwang <xrotwang@googlemail.com>
2019-10-04 07:51:44 +0200

    added skeleton

---
+++ lexibank_lindseyende.py
@@ -0,0 +1,30 @@
+from clldutils.path import Path
+from pylexibank.dataset import Dataset as BaseDataset
+
+
+class Dataset(BaseDataset):
+    dir = Path(__file__).parent
+    id = "lindseyende"
+
+    def cmd_download(self, **kw):
+        """
+        Download files to the raw/ directory. You can use helpers methods of `self.raw`, e.g.
+
+        >>> self.raw.download(url, fname)
+        """
+        pass
+
+    def cmd_install(self, **kw):
+        """
+        Convert the raw data to a CLDF dataset.
+
+        Use the methods of `pylexibank.cldf.Dataset` after instantiating one as context:
+
+        >>> with self.cldf as ds:
+        ...     ds.add_sources(...)
+        ...     ds.add_language(...)
+        ...     ds.add_concept(...)
+        ...     ds.add_lexemes(...)
+        """
+        with self.cldf as ds:
+            pass
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import sys
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_lindseyende',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_lindseyende'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'lindseyende=lexibank_lindseyende:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank>=1.1.1',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
