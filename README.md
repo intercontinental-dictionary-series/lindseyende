7b733f8b881de9c9b0b43ca87c22b88e480a89f6
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2019-10-04 15:32:07 +0200

    first implementation steps

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # lindseyende
+
+Cite the source dataset as
+
+> Lindsey, Kate Lynn .
+
+This dataset is licensed under a http://creativecommons.org/licenses/by/4.0/ license
+
+Available online at http://ids.clld.org
+
+Conceptlists in Concepticon:
+- [Key-2016-1310](http://concepticon.clld.org/contributions/Key-2016-1310)
--- lexibank_lindseyende.py
+++ lexibank_lindseyende.py
@@ -1,30 +1,80 @@
+import attr
 from clldutils.path import Path
 from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.dataset import Lexeme
 
+
+@attr.s
+class IDSLexeme(Lexeme):
+    Transcription = attr.ib(default=None)
+    AlternativeValue = attr.ib(default=None)
+    AlternativeTranscription = attr.ib(default=None)
+
+class IDSEntry:
+    def __init__(self, ids_id, form, alt_form, comment):
+        self.ids_id = ids_id
+        self.form = form
+        self.alt_form = alt_form
+        self.comment = comment
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "lindseyende"
 
-    def cmd_download(self, **kw):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw`, e.g.
+    lexeme_class = IDSLexeme
 
-        >>> self.raw.download(url, fname)
-        """
-        pass
+    def cmd_download(self, **kw):
+        self.raw.xls2csv("ids_cl_ende_final.xlsx")
 
     def cmd_install(self, **kw):
-        """
-        Convert the raw data to a CLDF dataset.
 
-        Use the methods of `pylexibank.cldf.Dataset` after instantiating one as context:
+        ccode = {
+            x.attributes["ids_id"]:
+                {
+                    "concepticon_id": x.concepticon_id,
+                    "concepticon_gloss": x.concepticon_gloss,
+                    "name": x.english,
+                }
+                for x in self.conceptlist.concepts.values()
+        }
 
-        >>> with self.cldf as ds:
-        ...     ds.add_sources(...)
-        ...     ds.add_language(...)
-        ...     ds.add_concept(...)
-        ...     ds.add_lexemes(...)
-        """
+        glottocode = "ende1235"
+        lang_id = "endepapuanewguinea"
+
         with self.cldf as ds:
-            pass
+            ds.add_sources(*self.raw.read_bib())
+
+            ds.add_language(
+                ID=lang_id,
+                Name="Ende (Papua New Guinea)",
+                Glottocode=glottocode,
+                # Latitude=-8.957653,
+                # Longitude=142.239116,
+            )
+
+            for form in self.read_csv():
+                ds.add_concept(
+                    ID=form.ids_id,
+                    Name=ccode[form.ids_id]["name"],
+                    Concepticon_ID=ccode[form.ids_id]["concepticon_id"],
+                    Concepticon_Gloss=ccode[form.ids_id]["concepticon_gloss"],
+                )
+                if form.form:
+                    ds.add_lexemes(
+                        Language_ID=lang_id,
+                        Parameter_ID=form.ids_id,
+                        Value=form.form,
+                        Comment=form.comment,
+                        Source="Lindsey2019",
+                        Transcription="standardorth",
+                        AlternativeValue=form.alt_form,
+                        AlternativeTranscription="phonetic",
+                    )
+
+    def read_csv(self, fname="ids_cl_ende_final.idsclldorg.csv"):
+        for i, row in enumerate(self.raw.read_csv(fname)):
+            row = [c.strip() for c in row[0:10]]
+            if i > 0:
+                row[0:2] = [int(float(c)) for c in row[0:2]]
+                entry = IDSEntry("%s-%s" % (row[0], row[1]), row[3], row[4], row[9])
+                yield entry
